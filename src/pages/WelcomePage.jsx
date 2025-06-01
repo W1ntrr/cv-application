77d1c8d96faffe61c111fcd5bbc7d31d5af996dc
W1ntrr <ryancatt390@gmail.com>
2025-05-31 23:49:01 -0400

    style: tweak welcome page design

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,11 +1,11 @@
-import { Link } from 'react-router-dom';
-import ResumeImage from '../assets/Resume.webp';
-import { ArrowUpRight, FileText } from 'lucide-react';
+import { Link } from "react-router-dom";
+import ResumeImage from "../assets/Resume.webp";
+import { ArrowUpRight, FileText } from "lucide-react";
 
 export default function WelcomePage() {
   return (
-    <div className="bg-slate-200 min-h-screen p-6 ">
-      <nav className="flex justify-between items-center mx-auto max-w-7xl">
+    <div className="bg-slate-200 min-h-screen ">
+      <nav className="flex justify-between items-center mx-auto max-w-8xl p-10">
         <Link to="/">
           <div className="text-3xl font-bold text-indigo-800 flex items-center ">
             <FileText className="inline-block w-10 h-10 mx-1" />
@@ -19,29 +19,32 @@ export default function WelcomePage() {
         </Link>
       </nav>
 
-      <main className="grid grid-cols-1 md:grid-cols-[55%_45%] max-w-7xl mx-auto mt-14 gap-16">
-        <div className="flex-1">
-          <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl md:text-6xl lg:text-7xl mb-6 max-w-2xl mt-16 leading-tight">
-            Build a professional resume
-          </h1>
-          <p className="text-slate-700 mb-8 text-lg sm:text-xl md:text-2xl tracking-wide opacity-80">
-            Quickly create a standout CV that highlights your skills — no hassle, all impact. 🚀
-          </p>
-          <button className="bg-indigo-950 text-xl text-white font-bold p-4 rounded-lg cursor-pointer shadow-md hover:scale-105 hover:bg-indigo-800 hover:shadow-lg transition-all duration-300 flex items-center w-full md:w-auto">
-            <ArrowUpRight className="inline-block w-10 h-10" />
-            <Link to="/resumeBuilder" className="mx-2">
-              Build Your Resume
-            </Link>
-          </button>
-        </div>
-        <div className="flex-1">
-          <img
-            src={ResumeImage}
-            alt="Resume"
-            className="w-full max-w-md mx-auto rounded-lg shadow-xl"
-          />
-        </div>
-      </main>
+      <div className="flex justify-center">
+        <section className="grid grid-cols-1 md:grid-cols-[55%_45%] max-w-full mt-10 gap-16 overflow-x-hidden px-16">
+          <div className="flex-1">
+            <h1 className="text-slate-900 font-extrabold text-5xl sm:text-6xl md:text-7xl lg:text-7xl xl:text-8xl mb-6 max-w-2xl mt-16 leading-tight">
+              Build a professional resume
+            </h1>
+            <p className="text-slate-700 mb-8 text-xl sm:text-2xl md:text-3xl tracking-wide opacity-80 text-balance">
+              Quickly create a standout CV that highlights your skills — no
+              hassle, all impact. 🚀
+            </p>
+            <button className="bg-indigo-950 text-xl text-white font-bold p-4 rounded-lg cursor-pointer shadow-md hover:scale-105 hover:bg-indigo-800 hover:shadow-lg transition-all duration-300 flex items-center w-full md:w-auto">
+              <ArrowUpRight className="inline-block w-10 h-10" />
+              <Link to="/resumeBuilder" className="mx-2">
+                Build Your Resume
+              </Link>
+            </button>
+          </div>
+          <div className="flex-1">
+            <img
+              src={ResumeImage}
+              alt="Resume"
+              className="w-full max-w-lg mx-auto rounded-lg shadow-xl"
+            />
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
